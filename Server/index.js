a7d93e09bd0b284b894ec271c7d1fcd164910d2f
KRISHNA GUPTA <krishnagupta110094@gmail.com>
2025-10-19 08:01:14 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,8 @@ app.use(express.json());
 app.use(fileUpload({ useTempFiles: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:"https://foodie-dun-ten.vercel.app/"
+    // origin: "http://localhost:5173",
     credentials: true,
   })
 );
